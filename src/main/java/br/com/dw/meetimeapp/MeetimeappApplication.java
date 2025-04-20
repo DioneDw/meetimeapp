551c7dbb285ea1b33656d3673eb6772502e01cf9
DioneDw <dionewilly@hotmail.com>
2025-04-20 13:10:26 -0300

    feat: create representation url in record and properties values

--- src/main/java/br/com/dw/meetimeapp/MeetimeappApplication.java
+++ src/main/java/br/com/dw/meetimeapp/MeetimeappApplication.java
@@ -2,8 +2,13 @@ package br.com.dw.meetimeapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import br.com.dw.meetimeapp.domain.record.AuthenticationUrlRecord;
 
 @SpringBootApplication
+@EnableConfigurationProperties(AuthenticationUrlRecord.class)
+
 public class MeetimeappApplication {
 
 	public static void main(String[] args) {
