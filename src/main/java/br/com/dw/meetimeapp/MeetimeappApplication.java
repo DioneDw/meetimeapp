308c00e0e794b5407927800f27481cd65312e87f
DioneDw <dionewilly@hotmail.com>
2025-04-20 11:56:42 -0300

    feat: started project

---
+++ src/main/java/br/com/dw/meetimeapp/MeetimeappApplication.java
@@ -0,0 +1,13 @@
+package br.com.dw.meetimeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetimeappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetimeappApplication.class, args);
+	}
+
+}
