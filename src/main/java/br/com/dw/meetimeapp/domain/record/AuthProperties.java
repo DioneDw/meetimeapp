1e41055e6af5eef1498c0f400287e9ce621b7c3c
DioneDw <dionewilly@hotmail.com>
2025-04-21 15:46:00 -0300

    feat: validation input properties with validation of spring

--- src/main/java/br/com/dw/meetimeapp/domain/record/AuthProperties.java
+++ src/main/java/br/com/dw/meetimeapp/domain/record/AuthProperties.java
@@ -1,8 +1,12 @@
 package br.com.dw.meetimeapp.domain.record;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import jakarta.validation.constraints.NotBlank;
 
 @ConfigurationProperties(prefix = "auth")
-public record AuthProperties(String clientId, String redirectUri, String authUrl, String scope) {
+@Validated
+public record AuthProperties(@NotBlank String clientId, @NotBlank String redirectUri, @NotBlank String authUrl, @NotBlank String scope) {
 
 }
--- src/main/java/br/com/dw/meetimeapp/domain/record/OAuthProperties.java
+++ src/main/java/br/com/dw/meetimeapp/domain/record/OAuthProperties.java
@@ -1,9 +1,13 @@
 package br.com.dw.meetimeapp.domain.record;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
+
+import jakarta.validation.constraints.NotBlank;
 
 @ConfigurationProperties(prefix = "oauth")
-public record OAuthProperties(String authUri, String grantType, String clientId, String clientSecret,
-        String redirectUri) {
+@Validated
+public record OAuthProperties(@NotBlank String authUri, @NotBlank String grantType, @NotBlank String clientId, @NotBlank String clientSecret,
+        @NotBlank String redirectUri) {
 
 }
