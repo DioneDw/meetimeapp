6db76b6de38e20ac72a776997520bcb71b2a7d88
DioneDw <dionewilly@hotmail.com>
2025-04-21 12:21:18 -0300

    feat: contact service and aux contact webclient for request

---
+++ src/main/java/br/com/dw/meetimeapp/client/ContactClient.java
@@ -0,0 +1,37 @@
+package br.com.dw.meetimeapp.client;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import br.com.dw.meetimeapp.domain.dto.ContactResponseDTO;
+import reactor.core.publisher.Mono;
+
+@Component
+public class ContactClient {
+
+    @Autowired
+    private WebClient webClient;
+
+    public ContactResponseDTO createContact(Map<String,Object> form, String token){
+        return webClient.post()
+        .uri("/crm/v3/objects/contacts")
+        .header(HttpHeaders.AUTHORIZATION, "Bearer " + token)
+        .contentType(MediaType.APPLICATION_JSON)
+        .bodyValue(form)
+        .retrieve()
+        .onStatus(
+            status -> status.isError(),
+            response -> response.bodyToMono(String.class)
+            .flatMap(error -> Mono.error(new RuntimeException("Erro ao criar contato no Hubspot:" +error)))
+        )
+        .bodyToMono(ContactResponseDTO.class)
+        .block();
+    }
+
+}
+
