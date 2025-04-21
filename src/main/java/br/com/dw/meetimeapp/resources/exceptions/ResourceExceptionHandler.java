9d6a3a69f33ac9540aa0bc398b081f27dade9408
DioneDw <dionewilly@hotmail.com>
2025-04-21 15:48:11 -0300

    feat: handler exception and return

---
+++ src/main/java/br/com/dw/meetimeapp/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,50 @@
+package br.com.dw.meetimeapp.resources.exceptions;
+
+import java.time.Instant;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import br.com.dw.meetimeapp.client.exceptions.HubspotCreateContactException;
+import br.com.dw.meetimeapp.client.exceptions.HubspotGetTokenException;
+import br.com.dw.meetimeapp.services.exceptions.InvalidCodeException;
+import br.com.dw.meetimeapp.services.exceptions.InvalidTokenException;
+import jakarta.servlet.http.HttpServletRequest;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+    
+    @ExceptionHandler(InvalidCodeException.class)
+    public ResponseEntity<StandardError> invalidCodeException(InvalidCodeException e, HttpServletRequest req){
+        String error = "Código informado nulo ou vazio!";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError response = new StandardError(Instant.now(),status.value(),error,e.getMessage(),req.getRequestURI());
+        return ResponseEntity.status(status).body(response);
+    }
+
+    @ExceptionHandler(InvalidTokenException.class)
+    public ResponseEntity<StandardError> invalidTokenException(InvalidTokenException e, HttpServletRequest req){
+        String error = "Token inválido ou mal formado!";
+        HttpStatus status = HttpStatus.BAD_REQUEST;
+        StandardError response = new StandardError(Instant.now(),status.value(),error,e.getMessage(),req.getRequestURI());
+        return ResponseEntity.status(status).body(response);
+    }
+
+    @ExceptionHandler(HubspotGetTokenException.class)
+    public ResponseEntity<StandardError> getTokenException(HubspotGetTokenException e){
+        String error = "Falha ao obter o token integração Hubspot!";
+        HttpStatus status = HttpStatus.BAD_GATEWAY;
+        StandardError response = new StandardError(Instant.now(),status.value(), error, e.getMessage());
+        return ResponseEntity.status(status).body(response);
+    }
+
+    @ExceptionHandler(HubspotCreateContactException.class)
+    public ResponseEntity<StandardError> createContactException(HubspotCreateContactException e){
+        String error = "Falha ao criar novo contato integração Hubspot!";
+        HttpStatus status = HttpStatus.BAD_GATEWAY;
+        StandardError response = new StandardError(Instant.now(),status.value(), error, e.getMessage());
+        return ResponseEntity.status(status).body(response);
+    }
+}
