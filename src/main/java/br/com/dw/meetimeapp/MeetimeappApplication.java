133c2de892e71ba5ee3404a12029a1696712f4ca
DioneDw <dionewilly@hotmail.com>
2025-04-23 20:46:42 -0300

    feat: ajusts and documentation

--- src/main/java/br/com/dw/meetimeapp/MeetimeappApplication.java
+++ src/main/java/br/com/dw/meetimeapp/MeetimeappApplication.java
@@ -1,14 +1,29 @@
 package br.com.dw.meetimeapp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 
 import br.com.dw.meetimeapp.domain.records.AuthProperties;
 import br.com.dw.meetimeapp.domain.records.OAuthProperties;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @SpringBootApplication
 @EnableConfigurationProperties({ AuthProperties.class, OAuthProperties.class })
+@OpenAPIDefinition(
+	info = @Info(title = "Teste técnico Meetime",
+	description = "Aplicação de integração de uma API Rest com o serviço Hubspot",
+	contact = @Contact(name="Dione", url = "http://github.com/DioneDw")
+	),
+	servers = {@Server(url = "http://localhost:8080", description = "Servidor Local")}
+)
 public class MeetimeappApplication {
 
 	public static void main(String[] args) {
